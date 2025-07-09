2b0708e293d45ebc4edf0163bf47bee87d13c6bc
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-07-09 12:09:04 +0530

    added error files

---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,19 @@
+class AppError extends Error{
+    constructor(
+            name,
+            message,
+            explanantion,
+            statusCode
+        )
+        {
+            super();
+    
+            this.name=name,
+            this.message=message,
+            this.explanantion=explanantion,
+            this.statusCode=statusCode
+        }
+    
+    }
+    
+    module.exports=AppError;
