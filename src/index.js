e98136c39d1a03aac1916a542c60619f3ff808b9
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-07-09 11:43:19 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const app=express();
+const {PORT}=require("../src/config/serverConfig");
+const apiRoutes=require("./routes/index");
+const db=require("./models/index");
+
+
+const setupAndStartServer=()=>{
+    app.listen(PORT,()=>{
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+        app.use("/api",apiRoutes);
+        console.log(`Server listening on ${PORT}`);
+        if(process.env.DB_SYNC)
+        {
+            db.sequelize.sync({alter:true});
+
+        }
+    });
+    
+}
+
+setupAndStartServer();
